cf94cef3f0d6cf2aa2b932c942a24cac60a845ff
ZataraHere <shabazekhan768@gmail.com>
2025-07-02 20:50:43 +0530

    Login and registration using Flask

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -13,6 +13,12 @@ def register():
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+@app.route('/login_validation', methods = ['POST'])
+def login_validation():
+    email = request.form.get("email")
+    password = request.form.get("password")
+    return  email, password
 
 if __name__ == "__main__":
     app.run(debug=True)
